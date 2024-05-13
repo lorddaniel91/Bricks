1f72b535b13f2f405085fe6aa20a6e676e92a25d
lorddaniel91 <156856589+lorddaniel91@users.noreply.github.com>
2024-05-13 10:36:58 +0200

    Update MapGenrator.java

--- src/main/MapGenrator.java
+++ src/main/MapGenrator.java
@@ -4,7 +4,7 @@ import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics2D;
 
-public class MapGenerator { //Térképgenerátor
+public class MapGenerator {
 	public int map[][];
 	public int brickWidth;
 	public int brickHeight;
